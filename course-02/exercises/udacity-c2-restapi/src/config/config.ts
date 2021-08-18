c68ab5bc3f62347f1f5119659bd38c3adcaddbdd
hakimks <hakimks@gmail.com>
2021-08-18 11:37:14 +0300

    added env variables

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "udagramhakimdev",
-    "password": "udagramhakimdev",
-    "database": "udagramhakimdev",
-    "host": "udagramhakimdev.chlejcfrlxip.us-east-1.rds.amazonaws.com",
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOSTNAME,
     "dialect": "postgres",
-    "aws_region": "us-east-1c",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagramhakim-dev"
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "prod": {
     "username": "",
